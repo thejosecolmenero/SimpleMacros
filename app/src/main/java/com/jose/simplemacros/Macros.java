b681e4075182dc2e24dae7e29a098304b23f51df
colmenerobnhs10 <jose.colmenero@tamu.edu>
2018-09-18 16:18:38 -0500

    initial GUI for Macro & BMR page completed

---
+++ app/src/main/java/com/jose/simplemacros/Macros.java
@@ -0,0 +1,13 @@
+package com.jose.simplemacros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Macros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_macros);
+    }
+}
--- app/src/main/java/com/jose/simplemacros/MainActivity.java
+++ app/src/main/java/com/jose/simplemacros/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jose.simplemacros;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void calc_bmr(View view){
+        Intent intent = new Intent(MainActivity.this, BMR.class);
+        startActivity(intent);
+    }
+
+
+    public void calc_macros(View view){
+        Intent intent = new Intent(MainActivity.this, Macros.class);
+        startActivity(intent);
+    }
+/*
+    public void calc_keto(View view){
+        Intent intent = new Intent(FromActivity.this, ToActivity.class);
+        startActivity(intent);
+    }
+    */
+
+
 }
